101a1c047d894de7522f513087cfa1afeebbe6e7
Mário Ferreira <marioo.netoo3@gmail.com>
2022-11-30 09:25:49 -0300

    atualização
    aprendendo CRUD no Entity Framework

--- Models/User.cs
+++ Models/User.cs
@@ -3,11 +3,11 @@ namespace Blog.Models
     public class User // tabela do sql
     {
         public int Id { get; set; } // coluna da tabela sql
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? PasswordHash { get; set; }
-        public string? Bio { get; set; }
-        public string? Image { get; set; }
-        public string? Slug { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public string Slug { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -6,8 +8,38 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            var ctx = new BlogDataContext();
-            ctx.Categories
+            using (var context = new BlogDataContext())
+            {
+                // var tag = new Tag { Name = ".NET", Slug = "dotnet"};
+
+                // context.Tags.Add(tag); // para salvar em memoria
+                // context.SaveChanges(); // para salvar a mudança no banco
+                // Console.WriteLine("Salvo");
+
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 3); // pegando do banco para atualizar
+                // tag.Name = "Ponto NET";
+                // tag.Slug = "dotnet";
+
+                // context.Update(tag);
+                // context.SaveChanges();
+                // Console.WriteLine("Atualizado!");
+
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // context.Remove(tag);
+                // context.SaveChanges();
+                // Console.WriteLine("Excluido");
+
+                // var tags = context.Tags; // nao executou no banco | o .ToList forçaria ser executada as tags
+
+                // foreach (var tag in tags) // aqui executou
+                // {
+                //     Console.WriteLine(tag.Name);
+                // }
+
+                // var tag = context.Tags.AsNoTracking().FirstOrDefault(x => x.Id == 3);
+                // Console.WriteLine(tag?.Name);
+            }
+
         }
     }
 }
